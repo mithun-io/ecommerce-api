c916e855ac56145d8586e8b1d104de5d1e269101
mithun-io <mithun-io@outlook.com>
2026-04-22 14:14:04 +0530

    login and password dtos

---
+++ src/main/java/com/ecommerce/dto/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.ecommerce.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email is not in format")
+    private String email;
+
+    @NotBlank(message = "password is required")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%&?:*/])(?=.*\\d)[A-Za-z!@#$%&?:*/\\d]{8,}$", message = "password must contain 8 characters with uppercase, lowercase, number and special character")
+    private String password;
+}
